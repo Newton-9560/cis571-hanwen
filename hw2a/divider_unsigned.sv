2983b1e767b6a51743c8826a6233d81b2785986b
Newton-9560 <149649322+Newton-9560@users.noreply.github.com>
2024-02-06 18:55:28 -0500

    hw2a

--- hw2a/divider_unsigned.sv
+++ hw2a/divider_unsigned.sv
@@ -11,7 +11,30 @@ module divider_unsigned (
     output wire [31:0] o_quotient
 );
 
-    // TODO: your code here
+  wire [31:0] divisor_tmp  [32:0];
+  wire [31:0] remainder_tmp[32:0];
+  wire [31:0] quotient_tmp [32:0];
+
+  assign divisor_tmp[0]   = i_dividend;
+  assign remainder_tmp[0] = 0;
+  assign quotient_tmp[0]  = 0;
+
+  genvar i;
+  for (i = 0; i < 32; i = i + 1) begin
+    divu_1iter d (
+        .i_dividend (divisor_tmp[i]),
+        .i_divisor  (i_divisor),
+        .i_quotient (quotient_tmp[i]),
+        .i_remainder(remainder_tmp[i]),
+        .o_dividend (divisor_tmp[i+1]),
+        .o_quotient (quotient_tmp[i+1]),
+        .o_remainder(remainder_tmp[i+1])
+    );
+  end
+
+  assign o_quotient  = quotient_tmp[32];
+  assign o_remainder = remainder_tmp[32];
+
 
 endmodule
 
@@ -25,19 +48,20 @@ module divu_1iter (
     output wire [31:0] o_remainder,
     output wire [31:0] o_quotient
 );
-  /*
-    for (int i = 0; i < 32; i++) {
-        remainder = (remainder << 1) | ((dividend >> 31) & 0x1);
-        if (remainder < divisor) {
-            quotient = (quotient << 1);
-        } else {
-            quotient = (quotient << 1) | 0x1;
-            remainder = remainder - divisor;
-        }
-        dividend = dividend << 1;
-    }
-    */
+  wire [31:0] remainder_tmp;
+  assign remainder_tmp = (i_remainder << 1) | ((i_dividend >> 31) & 32'h1);
+  // if(remainder_tmp < i_divisor) begin 
+  //     assign o_quotient = (i_quotient << 1);
+  //     assign o_remainder = remainder_tmp;
+  // end 
+  // else begin
+  //     assign o_quotient = (i_quotient << 1) | 32'h1;
+  //     assign o_remainder = remainder_tmp - i_divisor;
+  // end
 
-    // TODO: your code here
+  assign o_quotient = (remainder_tmp < i_divisor) ? (i_quotient << 1) : (i_quotient << 1) | 32'h1;
+  assign o_remainder = (remainder_tmp < i_divisor) ? remainder_tmp : remainder_tmp - i_divisor;
+
+  assign o_dividend = i_dividend << 1;
 
 endmodule
